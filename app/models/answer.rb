430e3593d81cdbb4aa0258cfce687c426ab30ffa
Samantha Holmes <samanthavholmes@gmail.com>
2016-10-09 16:41:47 -0400

    model/migration for answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,7 @@
 class Answer < ApplicationRecord
+  validates_presence_of :body
+
+  belongs_to :user
+  has_many :comments, as: :commentable
+  has_many :votes, as: :voteable
 end
--- db/migrate/20161009201531_create_answers.rb
+++ db/migrate/20161009201531_create_answers.rb
@@ -1,6 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
+      t.string :body, null: false
+      t.references :question, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
